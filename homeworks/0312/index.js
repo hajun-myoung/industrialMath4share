0d732a53ff9c33fcbbc605cd9fb900923cb9a850
Denver <fe.dev.denver@gmail.com>
2026-03-17 00:03:34 +0900

    [Create]0312 homeworks

---
+++ homeworks/0312/index.js
@@ -0,0 +1,37 @@
+// 2번
+const myStr = 'my industrial math class is great.';
+console.log(myStr.at(4));
+
+console.log(myStr.charCodeAt(4));
+
+console.log(myStr.includes('great'));
+console.log(myStr.includes('pizza'));
+
+console.log(myStr.endsWith('great'));
+console.log(myStr.endsWith('great.'));
+
+console.log(myStr.split());
+console.log(myStr.split('math'));
+console.log(myStr.split(' '));
+
+// 3번
+const price = 199_900;
+const quantity = 3;
+console.log(`총합은 ${price * quantity}원 입니다.`);
+
+// 4번
+const user = {
+  name: 'Hajun Myoung',
+  student_id: 2022270033,
+};
+
+console.log(user);
+
+// 5번
+const username = 'username';
+const email = 'email';
+const myObj = {
+  username,
+  email,
+};
+console.log(myObj);
