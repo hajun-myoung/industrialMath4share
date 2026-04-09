d4e0a062a4de0222ca3a5421606582b91f0219c4
Denver H. Myung <fe.dev.denver@gmail.com>
2026-04-09 12:03:52 +0900

    [Update]0402_hw.js

--- homeworks/0402_hw.js
+++ homeworks/0402_hw.js
@@ -1,14 +1,12 @@
 function generateRandomString(length = 10) {
-  return Math.random()
-    .toString(36)
-    .substring(2, 2 + length);
+  return Array.from({ length }, () => {
+    return Math.random().toString(36).substring(2, 3);
+  }).join('');
 }
 /**
  * NOTE: toString method returns just stringify something
  * But when it use 36 as a optinoal instance
  * this method returns 36-base incorded string transfer result
- *
- * FIXME: For now, more than 10 chracters' string cannot be generated
  */
 
 function generateRandomStringMinMax(min = 2, max = 10) {
@@ -17,12 +15,31 @@ function generateRandomStringMinMax(min = 2, max = 10) {
   return generateRandomString(length);
 }
 
-function generateMail() {
-  const mailMap = {
-    korea: 'ac.kr',
-    naver: 'com',
-    gmail: 'com',
-  };
+function generateMail(min = 2, max = 6) {
+  const mails = ['korea.ac.kr', 'naver.com', 'google.com'];
+  return `${generateRandomStringMinMax(min, max)}@${mails[Math.floor(Math.random() * 2)]}`;
+}
+
+/**
+ * @param {Array} array
+ * @param {Function} func
+ */
+function arrayFunc(array, func) {
+  const result = [];
+  for (const e of array) {
+    try {
+      result.push(func(e));
+    } catch {
+      return false;
+    }
+  }
+  return result;
+}
+
+function generatUsers(length = 2) {
+  return Array.from({ length }, () => {
+    return generateMail((max = 10));
+  });
 }
 
 // From here, test codes
@@ -39,3 +56,29 @@ console.log('generateRandomStringMinMax: ', generateRandomStringMinMax());
 console.log('generateRandomStringMinMax: ', generateRandomStringMinMax());
 console.log('generateRandomStringMinMax between 5 and 10: ', generateRandomStringMinMax(5, 10));
 console.groupEnd('generateRandomStringMinMax');
+
+console.group('gernerateMail');
+console.log('generateMail: ', generateMail());
+console.log('generateMail: ', generateMail());
+console.log('generateMail: ', generateMail());
+console.groupEnd('gernerateMail');
+
+console.group('array func');
+console.log(
+  'arrayFunc: ',
+  arrayFunc([1, 2, 3], (x) => x === 2),
+);
+console.log(
+  'arrayFunc: ',
+  arrayFunc([1, 2, 3, 4], (x) => {
+    if (x % 2 === 0) throw new Error('Even error');
+    return x;
+  }),
+);
+console.groupEnd('array func');
+
+console.group('generateMail');
+console.log('generatUsers', generatUsers());
+console.log('generatUsers', generatUsers(4));
+console.log('generatUsers', generatUsers(10));
+console.groupEnd('generateMail');
