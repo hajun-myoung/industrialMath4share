e41e9d52a867d183e8b4f5117633e3514dcac3df
Denver H. Myung <fe.dev.denver@gmail.com>
2026-05-10 23:47:03 +0900

    [Update]Week09.Project.Active tab Complete

--- project/public/js/order.js
+++ project/public/js/order.js
@@ -126,6 +126,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   // navigation bar - scrolling
   const tabButtons = document.querySelectorAll('.category');
+  const sections = [...document.getElementsByClassName('menu-section')];
+  let currentSection = sections[0].id;
 
   tabButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -141,21 +143,43 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   // active tabs
   /**
-   * TODO: NOTE:
+   * NOTE:
    * 기본 아이디어 - 현재 스크롤이 어느 섹션 근처인지 판단해서, 탭 active 하기
    * - [x] section들 가져오기
    * - [x] 전체 Area에 스크롤 이벤트 걸기(스크롤 할 때마다 훅)
    * - [x] 초기값은 첫 번째 섹션(아마 커피)로 지정
-   * - [ ] 아 어떻게 구현할지 헷갈려요 Next 쓰게해주세요
-   * - [ ] 자바스크립트는 못쓰겠어 타입스크립트를..제게...!
-   * - [ ] 내리다가 새로운 섹션 근처에 오면(딱 맞게보다 나을듯) active 탭 바꿔주기
+   * - [x] 아 어떻게 구현할지 헷갈려요 Next 쓰게해주세요
+   * - [x] 자바스크립트는 못쓰겠어 타입스크립트를..제게...!
+   * - [x] 내리다가 새로운 섹션 근처에 오면(딱 맞게보다 나을듯) active 탭 바꿔주기
    *    - 일단 한 40 정도로 해놓고, 손코딩하기
-   * - [ ] 신에게는 아직 12개의 논리오류가 있사옵니다
+   * - [x] 신에게는 아직 12개의 논리오류가 있사옵니다
+   *
+   * TODO: ISSUE:
+   * - "coffee" - "tea" 구성이라고 가정하면
+   * tea 밑으로 내려갈 때, coffee 밑이기도 해서
+   * 둘 다 자기 섹션이라고 주장함 -> 새로운 논리 필요
+   *
+   * -> 현재 위치보다 "위에 있는 섹션"들 중에서, "가장 아래 있는 것"을 찾기?
+   * 섹션 밑에서부터 점검하면서(reverse check)
+   * 현재 스크롤이 등장하면 끊기(break)
    */
-  const sections = document.getElementsByClassName('menu-section');
+  // const sections = [...document.getElementsByClassName('menu-section')]; // move up
   const scrollArea = document.querySelector('#menu-scroll-area');
 
+  // let currentSection = sections[0].id; // move up
   scrollArea.addEventListener('scroll', () => {
-    let currentSection = sections[0].id;
+    for (let idx = sections.length - 1; idx >= 0; idx--) {
+      const section = sections[idx];
+
+      if (scrollArea.scrollTop >= section.offsetTop - 120) {
+        currentSection = section.id;
+        console.log("it's me! ", section.id);
+        break;
+      }
+    }
+
+    tabButtons.forEach((button) => {
+      button.classList.toggle('active', button.dataset.target === currentSection);
+    });
   });
 });
