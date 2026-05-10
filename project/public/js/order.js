93e78690237ded0bd65ee2dc06110dc4fd8177af
Denver H. Myung <fe.dev.denver@gmail.com>
2026-05-10 22:24:14 +0900

    [Create]Week09.Project.MenuCards

--- project/public/js/order.js
+++ project/public/js/order.js
@@ -77,4 +77,50 @@ document.addEventListener('DOMContentLoaded', async () => {
   backButton.addEventListener('click', () => {
     window.location.href = '../index.html';
   });
+
+  // Main Sections
+  const menuArea = document.getElementById('menu-scroll-area');
+  categories.forEach((category) => {
+    const newSection = document.createElement('section');
+
+    const newHeader = document.createElement('h1');
+    newHeader.innerText = capitalizeWords(category);
+    newSection.appendChild(newHeader);
+
+    const includedMenus = menus.filter((menu) => menu.category === category);
+    // console.log(category, includedMenus);
+
+    const newMenusCardWrapper = document.createElement('div');
+    newMenusCardWrapper.className = 'menu-cards-wrapper';
+    includedMenus.forEach(async (menu) => {
+      const newMenuCard = document.createElement('div');
+      const newImageWrapper = document.createElement('div');
+      const newImageEle = document.createElement('img');
+      const newMenuName = document.createElement('div');
+      const newPrice = document.createElement('div');
+
+      newImageEle.src = menu.image;
+      newImageWrapper.appendChild(newImageEle);
+      newImageWrapper.className = 'menu-card-image';
+
+      const parsedName = await JSON.parse(menu.name);
+      newMenuName.innerText = parsedName[localStorage.getItem('selectedLanguage')];
+      newMenuName.className = 'menu-card-name';
+
+      newPrice.innerText = `₩${Number(menu.price).toLocaleString()}`;
+      newPrice.className = 'menu-card-price';
+
+      newMenuCard.className = 'menu-card';
+      newMenuCard.appendChild(newImageWrapper);
+      newMenuCard.appendChild(newMenuName);
+      newMenuCard.appendChild(newPrice);
+      newMenusCardWrapper.appendChild(newMenuCard);
+    });
+    newSection.appendChild(newMenusCardWrapper);
+
+    newSection.id = category.toLowerCase();
+    newSection.className = 'menu-section';
+
+    menuArea.appendChild(newSection);
+  });
 });
